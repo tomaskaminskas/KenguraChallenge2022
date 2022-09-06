e02965772620ef3a11a40f352c2ee49261e66f58
Tomas Kaminskas <kaminskas.tomas@gmail.com>
2022-09-06 10:19:32 +0000

    task2 init commit

--- Tomas/2021-task2.py
+++ Tomas/2021-task2.py
@@ -1,1 +1,15 @@
-print ('t')
+print ('Kengura 2021 Task 2')
+
+kids_facing = 'FBFFFBF'
+number_of_kids = len(kids_facing)
+i = 0
+answer = 0
+
+for kid in kids_facing:
+    if number_of_kids > i+1:
+        current = kids_facing[i]
+        next = kids_facing[i+1]
+        if current == 'F' and next == 'B':
+            answer += 1
+    i += 1
+print (answer)
